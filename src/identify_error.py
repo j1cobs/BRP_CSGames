6d487a12afd3c99ef161c8d194a31120e31ab0f4
fauverick <hungnc888@gmail.com>
2025-03-15 15:39:19 -0400

    add duplicate_number.json

--- src/identify_error.py
+++ src/identify_error.py
@@ -4,6 +4,9 @@ import json
 
 serverArr = []
 
+in_path = ".\data_input\servers"
+out_path = ".\data_output"
+
 apps =  {
  'Terraform': 'TFM',
  'Ad Manager Plus': 'ADP',
@@ -86,11 +89,50 @@ def analyzeServerName():
     f = open("../data_output/wrong_name.json", "w")
     f.write(fileContent)
     f.close()
-
+    
+def create_yaml_from_dict(output_filename,data):
+    with open(out_path+"/"+output_filename, 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
         
+def create_json_from_dict(output_filename,data):
+    with open(out_path+"/"+output_filename, 'w') as outfile:
+        json.dump(data, outfile,indent=4)
+
+def getDuplicateServerName():
+    serverDict = {
+    }
+
+    for server in serverArr:
+        serverDict[server.get("name")] = []
+
+
+    for i in range (1, 1307):
+        fileName = str(i)
+        while(len(fileName) < 8):
+            fileName = '0' + fileName
+        prunedFileName = fileName
+        fileName += '.yml'
+        fileName = "../data/servers/" + fileName
+
+        with open(fileName, 'r') as f:
+            data = yaml.full_load(f)
+        serverDict[data.get('name')].append(prunedFileName)
+
+    duplicateServerDict = {}
+
+    for server in serverDict :
+        if len(serverDict[server]) > 1:
+            duplicateServerDict[server] = serverDict[server]
+            # print(server)
+
+    with open("../data_output"+"/"+"duplicate_number.json", 'w') as outfile:
+        json.dump(duplicateServerDict, outfile,indent=4)
+
+
 def main():
     readServerFile()
     analyzeServerName()
+    getDuplicateServerName()
 
 main()
 
