29f64c7c2296c16f7f959c051dfa7a9492d57174
j1cobs <79954783+j1cobs@users.noreply.github.com>
2025-03-15 16:23:54 -0400

    Add incoming()

--- src/server_microseg.py
+++ src/server_microseg.py
@@ -1,2 +1,25 @@
 import server_inventory
 
+def incoming():
+    
+    files = server_inventory.loop_files()
+    
+    incoming={}
+    
+    for file in files:
+        
+        name = file["name"]
+        
+        for port,servers in file["outgoing"].items():
+            for server in servers:
+                if server not in incoming.keys():
+                    incoming[server]= {name:[port]}
+                elif name not in incoming[server]:
+                    incoming[server][name]=[port]
+                elif port not in incoming[server][name]:
+                    incoming[server][name].append(port)
+    return incoming
+
+server_inventory.create_json_from_dict("incoming.json",incoming())
+    
+# incoming()
